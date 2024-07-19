edd901872a64b98263257263a50f3a771c90b371
LuisFernandoRojasSanchez2 <luisfernandorojassanchez1@gmail.com>
2024-07-19 15:12:51 -0600

    Create Graded_Assesment_Week4_3.py

---
+++ Week4/Graded_Assesment_Week4_3.py
@@ -0,0 +1,14 @@
+# Currently there is a string called str1. Write code to create a list called chars which should contain the characters from str1.
+# Each character in str1 should be its own element in the list chars.
+
+str1 = "I love python"
+# HINT: what's the accumulator? That should go here.
+
+chars = []
+
+for char in str1:
+    chars.append(char)
+
+print(chars)
+
+#This is the only problem I could find online from this course because I had already taken it and I want to save the 7 day free trial for later courses that are more difficult.
